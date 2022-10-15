bc293bc0f82eaf5d963b06b3b3f74af90e43473b
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-15 11:48:51 +0300

    1st ruby

---
+++ app.rb
@@ -0,0 +1,54 @@
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# parentheses is optional in ruby
+# puts("Hello world!")
+
+# to run ruby we do it on the terminal and { ruby filename.rb}
+
+# Ruby several methods for printing output to the terminal: puts, print
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# the only difference is that puts will add a line break at the end of the string. 
+
+# {puts == Hello world!
+# Hello world!
+# Hello world! }
+
+# {print == Hello world!Hello world!Hello world! }
+
+# puts [1, 2, 3]
+
+#  p is a method for data inspection {calling the .inspect method on our data.}
+# p [1, 2, 3]
+
+# method, pp, for "pretty-printing" complex data == nested arrays and hashes 
+# pp {by calling the .pretty_inspect method on our data.}
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# Ruby comes with a tool called IRB (Interactive Ruby) for running a Ruby REPL (read-evaluate-print-loop) in the terminal == for debugging // its just like console.log of js ==>{irb}
+
+#  => nil { ==> this line is the return value of the puts method}
+
+# in ruby => symbol returns a value , nil ==> is a no value and all methods have a return value
+
+#  *****===> the puts method always returns nil <===******  #
+
+# ## ****** In Ruby, it's convention to use underscores (_) to separate words in variables. This is referred to as snake case  ****** ## #
+
+#  RSpec Tests  =>RSpec is a Ruby gem (the Ruby equivalent of a npm package) that provides a domain-specific language
+
+
+#  learn-lab test is what works for me 
+# puts "Hello world!"
+# puts "Hello world!"
+puts "Hello World!"
+
+# print "Hello world!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
